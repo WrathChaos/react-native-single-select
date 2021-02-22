9a82c33182f1f21577d259b3a1b052a533ec7776
Mario Murrent <mario@murrent.at>
2021-02-22 16:22:38 +0100

    Updated library and README

--- lib/components/Icon.tsx
+++ lib/components/Icon.tsx
@@ -9,58 +9,59 @@ import defaultLightIconImage from "./down-arrow-light.png";
 import defaultDarkIconImage from "./down-arrow-dark.png";
 
 export type Source = {
-  uri?: string;
+    uri?: string;
 };
 
 interface IProps {
-  theme: string;
-  style?: ImageStyle | ImageStyle[];
-  iconImageSource?: Source | number;
+    theme: string;
+    style?: ImageStyle | ImageStyle[];
+    iconImageSource?: Source | number;
 }
 
 interface IState {
-  toggled: boolean;
-  rotation: Animated.Value;
+    toggled: boolean;
+    rotation: Animated.Value;
 }
 
 class Icon extends React.Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
-    this.state = {
-      toggled: false,
-      rotation: new Animated.Value(0),
+    constructor(props: IProps) {
+        super(props);
+        this.state = {
+            toggled: false,
+            rotation: new Animated.Value(0),
+        };
+    }
+
+    onPressAnimation = () => {
+        const { rotation, toggled } = this.state;
+        Animated.timing(rotation, {
+            toValue: toggled ? 0 : 1,
+            duration: 850,
+            easing: Easing.bounce,
+            useNativeDriver: true,
+        }).start(() => this.setState({ toggled: !this.state.toggled }));
     };
-  }
 
-  onPressAnimation = () => {
-    const { rotation, toggled } = this.state;
-    Animated.timing(rotation, {
-      toValue: toggled ? 0 : 1,
-      duration: 850,
-      easing: Easing.bounce,
-      useNativeDriver: true,
-    }).start(() => this.setState({ toggled: !this.state.toggled }));
-  };
-
-  render() {
-    const { style, iconImageSource, ...other } = this.props;
-    const rotate = this.state.rotation.interpolate({
-      inputRange: [0, 1],
-      outputRange: ["0deg", "180deg"],
-    });
-    return (
-      <Animated.Image
-        {...other}
-        source={
-          iconImageSource ||
-          (this.props.theme === DARK
-            ? defaultLightIconImage
-            : defaultDarkIconImage)
-        }
-        style={[style, { transform: [{ rotate }] }]}
-      />
-    );
-  }
+    render() {
+        const { style, iconImageSource, ...other } = this.props;
+        const rotate = this.state.rotation.interpolate({
+            inputRange: [0, 1],
+            outputRange: ["0deg", "180deg"],
+        });
+        return (
+            <Animated.Image
+                {...other}
+                // @ts-ignore
+                source={
+                    iconImageSource ||
+                    (this.props.theme === DARK
+                        ? defaultLightIconImage
+                        : defaultDarkIconImage)
+                }
+                style={[style, { transform: [{ rotate }] }]}
+            />
+        );
+    }
 }
 
 export default Icon;
