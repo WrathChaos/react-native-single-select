f0474bd3af0eab2a747f3929ff2d9f3b02de1ce7
Mario Murrent <mario@murrent.at>
2021-02-22 20:03:51 +0100

    Reverted to old code style

--- lib/RNSingleSelect.tsx
+++ lib/RNSingleSelect.tsx
@@ -68,120 +68,120 @@ interface IProps {
 let iconRef: any = undefined;
 const RNSingleSelect = (props: IProps) => {
   const {
-      data,
-      width,
-      height,
-      darkMode,
-      onSelect,
-      placeholder,
-      onTextChange,
-      arrowImageStyle,
-      animatedBorderRadius,
-      placeholderTextColor,
-      buttonContainerStyle,
-      placeholderTextStyle,
-      menuBarContainerStyle,
-      menuBarContainerHeight = 150,
-      menuBarContainerWidth = 250,
-      disableAbsolute = false,
-      ImageComponent = Image,
-      TextComponent = Text,
-      disableFilterAnimation = false,
-      spinnerType = "ThreeBounce",
-      spinnerSize = 30,
-      spinnerColor,
-      editable = true
+    data,
+    width,
+    height,
+    darkMode,
+    onSelect,
+    placeholder,
+    onTextChange,
+    arrowImageStyle,
+    animatedBorderRadius,
+    placeholderTextColor,
+    buttonContainerStyle,
+    placeholderTextStyle,
+    menuBarContainerStyle,
+    menuBarContainerHeight = 150,
+    menuBarContainerWidth = 250,
+    disableAbsolute = false,
+    ImageComponent = Image,
+    TextComponent = Text,
+    disableFilterAnimation = false,
+    spinnerType = "ThreeBounce",
+    spinnerSize = 30,
+    spinnerColor,
+    editable = true
   } = props;
 
   const [
-      selectedItem,
-      setSelectedItem,
+    selectedItem,
+    setSelectedItem,
   ] = React.useState<ISingleSelectDataType | null>(null);
   const [menuToggled, setMenuToggled] = React.useState<boolean | null>(false);
   const [dataBackup, setDataBackup] = React.useState<
-      Array<ISingleSelectDataType> | undefined
-      >(data);
+    Array<ISingleSelectDataType> | undefined
+    >(data);
   const [dataSource, setDataSource] = React.useState<
-      Array<ISingleSelectDataType> | undefined
-      >(data);
+    Array<ISingleSelectDataType> | undefined
+    >(data);
   const [borderRadiusAnimation, setBorderRadiusAnimation] = React.useState<
-      Animated.Value
-      >(new Animated.Value(animatedBorderRadius || 16));
+    Animated.Value
+    >(new Animated.Value(animatedBorderRadius || 16));
   const [
-      menuBarYTranslateAnimation,
-      setMenuBarYTranslateAnimation,
+    menuBarYTranslateAnimation,
+    setMenuBarYTranslateAnimation,
   ] = React.useState<Animated.Value>(new Animated.Value(0));
   const [theme, setTheme] = React.useState(DARK);
 
   React.useEffect(() => {
-      if (darkMode) setTheme(DARK);
-      else setTheme(LIGHT);
+    if (darkMode) setTheme(DARK);
+    else setTheme(LIGHT);
   }, []);
 
   React.useEffect(() => {
-      setDataSource(data);
-      setDataBackup(data);
+    setDataSource(data);
+    setDataBackup(data);
   }, [data]);
 
   const animateBorderRadius = () => {
-      Animated.timing(borderRadiusAnimation, {
-          toValue: menuToggled ? animatedBorderRadius || 16 : 0,
-          duration: 1250,
-          easing: Easing.bounce,
-          useNativeDriver: true,
-      }).start();
+    Animated.timing(borderRadiusAnimation, {
+      toValue: menuToggled ? animatedBorderRadius || 16 : 0,
+      duration: 1250,
+      easing: Easing.bounce,
+      useNativeDriver: true,
+    }).start();
   };
 
   const animateSelectionBar = () => {
-      Animated.timing(menuBarYTranslateAnimation, {
-          toValue: menuToggled ? 0 : 100,
-          duration: 250,
-          easing: Easing.ease,
-          useNativeDriver: true,
-      }).start();
+    Animated.timing(menuBarYTranslateAnimation, {
+      toValue: menuToggled ? 0 : 100,
+      duration: 250,
+      easing: Easing.ease,
+      useNativeDriver: true,
+    }).start();
   };
 
   const handleOnToggleMenuBar = (isMenuToggled?: boolean) => {
-      iconRef?.onPressAnimation();
-      animateBorderRadius();
-      animateSelectionBar();
-      setMenuToggled(isMenuToggled ? isMenuToggled : !menuToggled);
+    iconRef?.onPressAnimation();
+    animateBorderRadius();
+    animateSelectionBar();
+    setMenuToggled(isMenuToggled ? isMenuToggled : !menuToggled);
   };
 
   const handleOnSelectItem = (item: ISingleSelectDataType) => {
-      handleOnFilter("");
-      setSelectedItem(item);
-      handleOnToggleMenuBar();
-      onSelect && onSelect(item);
+    handleOnFilter("");
+    setSelectedItem(item);
+    handleOnToggleMenuBar();
+    onSelect && onSelect(item);
   };
 
   const triggerFilterAnimation = () => {
-      LayoutAnimation.configureNext({
-          duration: 1000,
-          create: {
-              type: LayoutAnimation.Types.spring,
-              property: LayoutAnimation.Properties.opacity,
-              springDamping: 1,
-          },
-          delete: {
-              type: LayoutAnimation.Types.spring,
-              property: LayoutAnimation.Properties.opacity,
-              springDamping: 1,
-          },
-      });
+    LayoutAnimation.configureNext({
+      duration: 1000,
+      create: {
+        type: LayoutAnimation.Types.spring,
+        property: LayoutAnimation.Properties.opacity,
+        springDamping: 1,
+      },
+      delete: {
+        type: LayoutAnimation.Types.spring,
+        property: LayoutAnimation.Properties.opacity,
+        springDamping: 1,
+      },
+    });
   };
 
   const handleOnFilter = (text: string) => {
-      let newData = dataBackup;
-      newData = dataBackup?.filter((item) => {
-          const itemData = item.value.toLowerCase();
-          const textData = text.toLowerCase();
-          return itemData.indexOf(textData) > -1;
-      });
-      !disableFilterAnimation && triggerFilterAnimation();
-      setDataSource(newData);
-      setSelectedItem({ value: text });
-      setDataSource(newData);
+    let newData = dataBackup;
+    newData = dataBackup?.filter((item) => {
+      const itemData = item.value.toLowerCase();
+      const textData = text.toLowerCase();
+      return itemData.indexOf(textData) > -1;
+    });
+    !disableFilterAnimation && triggerFilterAnimation();
+    setDataSource(newData);
+    setSelectedItem({ value: text });
+    setDataSource(newData);
   };
 
   /* -------------------------------------------------------------------------- */
@@ -189,128 +189,128 @@ const RNSingleSelect = (props: IProps) => {
   /* -------------------------------------------------------------------------- */
 
   const renderSingleSelectButton = () => {
-      return (
-          <TouchableOpacity
-              {...props}
-              onPress={() => {
-                  handleOnToggleMenuBar();
-              }}
-          >
-              <Animated.View
-                  style={[
-                      _menuButtonContainer(theme, height, width),
-                      {
-                          borderRadius: borderRadiusAnimation,
-                      },
-                      buttonContainerStyle,
-                  ]}
-              >
-                  <View style={styles.buttonContainerGlue}>
-                      <TextInput
-                          editable={editable}
-                          placeholderTextColor={
-                              placeholderTextColor
-                                  ? placeholderTextColor
-                                  : selectedItem
-                                  ? ThemeColors[theme].textColor
-                                  : ThemeColors[theme].placeholderColor
-                          }
-                          style={[
-                              _placeholderTextStyle(theme, selectedItem),
-                              placeholderTextStyle,
-                          ]}
-                          placeholder={placeholder || "Select"}
-                          onFocus={() => handleOnToggleMenuBar(false)}
-                          onChangeText={(text: string) => {
-                              if (text.length === 0) handleOnFilter("");
-                              else handleOnFilter(text);
-                              onTextChange && onTextChange(text);
-                          }}
-                      >
-                          <TextComponent>{selectedItem?.value}</TextComponent>
-                      </TextInput>
-                      <Icon
-                          theme={theme}
-                          ref={(ref: Icon) => (iconRef = ref)}
-                          style={[styles.arrowImageStyle, arrowImageStyle]}
-                          {...props}
-                      />
-                  </View>
-              </Animated.View>
-          </TouchableOpacity>
-      );
+    return (
+        <TouchableOpacity
+            {...props}
+            onPress={() => {
+                handleOnToggleMenuBar();
+            }}
+        >
+            <Animated.View
+                style={[
+                    _menuButtonContainer(theme, height, width),
+                    {
+                        borderRadius: borderRadiusAnimation,
+                    },
+                    buttonContainerStyle,
+                ]}
+            >
+                <View style={styles.buttonContainerGlue}>
+                    <TextInput
+                        editable={editable}
+                        placeholderTextColor={
+                            placeholderTextColor
+                                ? placeholderTextColor
+                                : selectedItem
+                                ? ThemeColors[theme].textColor
+                                : ThemeColors[theme].placeholderColor
+                        }
+                        style={[
+                            _placeholderTextStyle(theme, selectedItem),
+                            placeholderTextStyle,
+                        ]}
+                        placeholder={placeholder || "Select"}
+                        onFocus={() => handleOnToggleMenuBar(false)}
+                        onChangeText={(text: string) => {
+                            if (text.length === 0) handleOnFilter("");
+                            else handleOnFilter(text);
+                            onTextChange && onTextChange(text);
+                        }}
+                    >
+                        <TextComponent>{selectedItem?.value}</TextComponent>
+                    </TextInput>
+                    <Icon
+                        theme={theme}
+                        ref={(ref: Icon) => (iconRef = ref)}
+                        style={[styles.arrowImageStyle, arrowImageStyle]}
+                        {...props}
+                    />
+                </View>
+            </Animated.View>
+        </TouchableOpacity>
+    );
   };
 
   const renderMenuItem = (menuItem: any) => {
-      const { index } = menuItem;
-      const { id, value, imageSource } = menuItem.item;
-      const { data, imageWidth, imageHeight, menuItemTextStyle } = props;
-      return (
-          <TouchableHighlight
-              key={id}
-              style={_menuItemContainer(index, data)}
-              onPress={() => {
-                  handleOnSelectItem(menuItem.item);
-              }}
-          >
-              <View style={styles.menuBarItemContainerGlue}>
-                  {imageSource && (
-                      <ImageComponent
-                          resizeMode="contain"
-                          source={imageSource}
-                          style={_imageStyle(imageHeight, imageWidth)}
-                      />
-                  )}
-                  <TextComponent style={[_menuItemTextStyle(theme), menuItemTextStyle]}>
-                      {value}
-                  </TextComponent>
-              </View>
-          </TouchableHighlight>
-      );
+    const { index } = menuItem;
+    const { id, value, imageSource } = menuItem.item;
+    const { data, imageWidth, imageHeight, menuItemTextStyle } = props;
+    return (
+        <TouchableHighlight
+            key={id}
+            style={_menuItemContainer(index, data)}
+            onPress={() => {
+                handleOnSelectItem(menuItem.item);
+            }}
+        >
+            <View style={styles.menuBarItemContainerGlue}>
+                {imageSource && (
+                    <ImageComponent
+                        resizeMode="contain"
+                        source={imageSource}
+                        style={_imageStyle(imageHeight, imageWidth)}
+                    />
+                )}
+                <TextComponent style={[_menuItemTextStyle(theme), menuItemTextStyle]}>
+                    {value}
+                </TextComponent>
+            </View>
+        </TouchableHighlight>
+    );
   };
 
   const renderSpinner = () => (
-      <View style={styles.spinnerContainer}>
-          <Spinner
-              size={spinnerSize}
-              type={spinnerType}
-              color={spinnerColor || ThemeColors[theme].textColor}
-              isVisible={!(dataSource && dataSource.length > 0)}
-          />
-      </View>
+    <View style={styles.spinnerContainer}>
+      <Spinner
+        size={spinnerSize}
+        type={spinnerType}
+        color={spinnerColor || ThemeColors[theme].textColor}
+        isVisible={!(dataSource && dataSource.length > 0)}
+      />
+    </View>
   );
 
   const renderList = () => (
-      <FlatList
-          data={dataSource}
-          style={styles.listStyle}
-          renderItem={renderMenuItem}
-      />
+    <FlatList
+      data={dataSource}
+      style={styles.listStyle}
+      renderItem={renderMenuItem}
+    />
   );
 
   const renderMenuBar = () => {
-      const rotate = menuBarYTranslateAnimation.interpolate({
-          inputRange: [0, 25, 50, 75, 100],
-          outputRange: [0, 0.5, 0.75, 0.9, 1],
-      });
-      return (
-          <Animated.View
-              style={[
-                  _menuBarContainer(
-                      theme,
-                      menuBarContainerHeight,
-                      menuBarContainerWidth,
-                  ),
-                  {
-                      transform: [{ scaleY: rotate }],
-                      display: disableAbsolute ? "flex" : menuToggled ? "flex" : "none",
-                  },
-                  menuBarContainerStyle,
-              ]}
-          >
-              {dataSource && dataSource.length > 0 ? renderList() : renderSpinner()}
-          </Animated.View>
-      );
+    const rotate = menuBarYTranslateAnimation.interpolate({
+      inputRange: [0, 25, 50, 75, 100],
+      outputRange: [0, 0.5, 0.75, 0.9, 1],
+    });
+    return (
+      <Animated.View
+        style={[
+          _menuBarContainer(
+            theme,
+            menuBarContainerHeight,
+            menuBarContainerWidth,
+          ),
+          {
+            transform: [{ scaleY: rotate }],
+            display: disableAbsolute ? "flex" : menuToggled ? "flex" : "none",
+          },
+          menuBarContainerStyle,
+        ]}
+      >
+        {dataSource && dataSource.length > 0 ? renderList() : renderSpinner()}
+      </Animated.View>
+    );
   };
 
   return (
--- lib/components/Icon.tsx
+++ lib/components/Icon.tsx
@@ -27,38 +27,38 @@ class Icon extends React.Component<IProps, IState> {
   constructor(props: IProps) {
     super(props);
     this.state = {
-        toggled: false,
-        rotation: new Animated.Value(0),
+      toggled: false,
+      rotation: new Animated.Value(0),
     };
   }
 
   onPressAnimation = () => {
     const { rotation, toggled } = this.state;
     Animated.timing(rotation, {
-        toValue: toggled ? 0 : 1,
-        duration: 850,
-        easing: Easing.bounce,
-        useNativeDriver: true,
+      toValue: toggled ? 0 : 1,
+      duration: 850,
+      easing: Easing.bounce,
+      useNativeDriver: true,
     }).start(() => this.setState({ toggled: !this.state.toggled }));
   };
 
   render() {
     const { style, iconImageSource, ...other } = this.props;
     const rotate = this.state.rotation.interpolate({
-        inputRange: [0, 1],
-        outputRange: ["0deg", "180deg"],
+      inputRange: [0, 1],
+      outputRange: ["0deg", "180deg"],
     });
     return (
-        <Animated.Image
-            {...other}
-            source={
-                iconImageSource ||
-                (this.props.theme === DARK
-                    ? defaultLightIconImage
-                    : defaultDarkIconImage)
-            }
-            style={[style, { transform: [{ rotate }] }]}
-        />
+      <Animated.Image
+        {...other}
+        source={
+          iconImageSource ||
+          (this.props.theme === DARK
+            ? defaultLightIconImage
+            : defaultDarkIconImage)
+        }
+        style={[style, { transform: [{ rotate }] }]}
+      />
     );
   }
 }
